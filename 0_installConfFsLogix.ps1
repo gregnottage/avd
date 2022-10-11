6db03abdd3a0cbe1c18aac36a9eb5df5a65b4b7a
gregnottage <64895572+gregnottage@users.noreply.github.com>
2022-10-11 16:42:03 +0100

    Update 0_installConfFsLogix.ps1

--- 0_installConfFsLogix.ps1
+++ 0_installConfFsLogix.ps1
@@ -7,15 +7,7 @@ $outputPath = $LocalPath + '\' + $WVDFslogixInstaller
 Invoke-WebRequest -Uri $WVDflogixURL -OutFile $outputPath
 set-Location $LocalPath
 
-$fsLogixURL = "https://aka.ms/fslogix_download"
-$installerFile = "fslogix_download.zip"
-
-Invoke-WebRequest $fsLogixURL -OutFile $LocalPath\$installerFile
-Expand-Archive $LocalPath\$installerFile -DestinationPath $LocalPath
-write-host 'AIB Customization: Download Fslogix installer finished'
-
 write-host 'AIB Customization: Start Fslogix installer'
-#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Verbose
 .\\FSLogixSetup.ps1 -ProfilePath \\wvdSMB\wvd -Verbose
 write-host 'AIB Customization: Finished Fslogix installer'
